363d0b73fac5a045745cee5454bcbc82f5c13315
KJ Price <kjprice12@gmail.com>
2020-01-22 17:06:44 -0600

    Class 1

---
+++ class1/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(480, 500);
+
+  console.log('Hello World');
+}
+
+function draw() {
+    background(204);
+
+    const width = '50';
+
+    ellipse(width, 50, 80, 80);
+}
